76a4418790ad934009417d7e03b7925297fdfd57
tsin <chunfeng_tsin@163.com>
2022-05-24 11:25:22 +0800

    draw point in tag image.

--- lesson-0/Main.cpp
+++ lesson-0/Main.cpp
@@ -1,3 +1,20 @@
 //
 // Created by tsin on 22-5-24.
 //
+
+#include "tgaimage.h"
+ const TGAColor red = TGAColor(255, 0, 0, 255);
+
+int main(){
+    TGAImage image(100, 100, TGAImage::RGBA);
+    //   |
+    //   |
+    //   |
+    //   |
+    //   |
+    //   |
+    // (0, 0)________________________
+    image.set(10, 20, red);
+    image.write_tga_file("out.tga");
+    return 0;
+}
